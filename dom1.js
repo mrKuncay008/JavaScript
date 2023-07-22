60ab9e1e390a4917f357208dcf32ec5930d0e7ac
tejho <tejho@localhost.localdomain>
2023-07-22 12:44:21 +0700

    Update file 22 juli

--- dom1.js
+++ dom1.js
@@ -2,6 +2,19 @@ let kucing = {
     Aisma: ["Pony","Boy","Ratu"],
     Masse: ["Kuncay","Popy"]
 }
-
+// Menampilkan array kucing
 console.log(kucing)
-console.log(kucing[Aisma])
+// Menampilkan  Array Kucing objrct Aisma
+console.log(kucing["Aisma"]) // Dengan array
+
+console.log("Kucing Aisma: ")
+console.log(kucing.Aisma.join()) // Dengan Join
+
+// Menampilkan arrray Kucing dengan object Masse
+console.log("Kucing Masse",kucing["Masse"]) // denga array
+console.log("Kucing Masse: ")
+console.log(kucing.Masse.join()) // Dengan Join
+console.log("Kucing Aisma Mati satu Namanya:", kucing.Aisma[2])
+
+console.log("Kucing aisma")
+kucing.Aisma.forEach(aisma => console.log(aisma)); // Dengan Foreach
