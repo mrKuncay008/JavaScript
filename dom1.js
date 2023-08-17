78a154826ff50b444671fea867f69d6c414f9c38
mrKuncay008 <kuncaymuhammad@gmail.com>
2023-08-17 21:24:45 +0700

    Update 17 agust

--- dom1.js
+++ dom1.js
@@ -3,18 +3,18 @@ let kucing = {
     Masse: ["Kuncay","Popy"]
 }
 // Menampilkan array kucing
-console.log(kucing)
+console.log(kucing);
 // Menampilkan  Array Kucing objrct Aisma
-console.log(kucing["Aisma"]) // Dengan array
+console.log(kucing["Aisma"]); // Dengan array
 
-console.log("Kucing Aisma: ")
-console.log(kucing.Aisma.join()) // Dengan Join
+console.log("Kucing Aisma: ");
+console.log(kucing.Aisma.join()); // Dengan Join
 
 // Menampilkan arrray Kucing dengan object Masse
-console.log("Kucing Masse",kucing["Masse"]) // denga array
-console.log("Kucing Masse: ")
-console.log(kucing.Masse.join()) // Dengan Join
-console.log("Kucing Aisma Mati satu Namanya:", kucing.Aisma[2])
+console.log("Kucing Masse",kucing["Masse"]); // denga array
+console.log("Kucing Masse: ");
+console.log(kucing.Masse.join()); // Dengan Join
+console.log("Kucing Aisma Mati satu Namanya:", kucing.Aisma[2]);
 
-console.log("Kucing aisma")
+console.log("Kucing aisma");
 kucing.Aisma.forEach(aisma => console.log(aisma)); // Dengan Foreach
