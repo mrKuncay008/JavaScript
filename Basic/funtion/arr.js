a9dd4cad9541df895a267f88dab05bf7c43a09cd
mrKuncay008 <kuncaymuhammad@gmail.com>
2023-09-25 21:19:09 +0700

    Update 25 sept

--- Basic/funtion/arr.js
+++ Basic/funtion/arr.js
@@ -1,14 +1,14 @@
 function arrObjt(Bar) {
     var hasil = [];
-    var obj = {};
 
     for (let i = 0; i < Bar.length; i++) {
+        C
         obj.Tipe = Bar[i][1];
         obj.Nama = Bar[i][0];
         obj.Harga = Bar[i][2];
         hasil.push(Bar);
-        bar = {};
     }
     return hasil;
-    console.log(Bar(['Montong','Kuncay','90000']));
 }
+var Bar = [['Montong','Kuncay','90000']];
+console.log(arrObjt[Bar]);
