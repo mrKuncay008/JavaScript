00a1671b05e0604aaa7057a8556d41b9727cd63d
mrKuncay008 <kuncaymuhammad@gmail.com>
2023-11-04 11:15:49 +0700

    Update 04 nov

---
+++ angkotpart2/src/App.js
@@ -0,0 +1,17 @@
+import logo from './lol.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hallo Welcome a Angkot Project 😊 ! Cheerss 🥶 !
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
