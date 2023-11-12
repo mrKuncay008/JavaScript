8a8a4fccdac4f7a8e65266fdd64371166ec97494
mrKuncay008 <kuncaymuhammad@gmail.com>
2023-11-12 14:15:05 +0700

    Update 12 Nov

--- angkotpart2/src/Comp/seet.js
+++ angkotpart2/src/Comp/seet.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import KurSatu from '../Property/kursi.png';
+
 const Seet = (props) => {
 
 const algo = () => {
   alert("Kursi Masih Kosong");
 }
-const test = () => {
-  const kucing = ['popy', 'oren', 'kuncay'];
-  console.log(kucing)
-}
-    // var Penumpang = [];
 
-    // var dataPenump = document.getElementById('tambah');
-    // var input = document.getElementById('penumpang');
-    // var output1 = document.getElementById('kotak1');
-    // var output2 = document.getElementById('kotak2');
-    // var output3 = document.getElementById('kotak3');
-  // nanti algoritma java nya menggunakan Prop
-  return (
-<div className="Area">
+const test = () => {
+  var kucing = [];
+  function tambah(nama, kucing) {
+    if (kucing.length === 0) {
+  		// maka di tambah penumpang
+  		kucing.push(nama);
+  		// kembalikan nila
+  		return kucing;
+    }
+  }
+}
+return (
+  <div className="Area">
   {test()}
-  {algo()}
   <div className="supir" />
-  <img src={KurSatu} alt="Kursi 1" className="kotak1" id='kotak1' onClick={algo} />
-  <img src={KurSatu} alt="Kursi 1" className="kotak2" id='kotak2' onClick={algo} />
-  <img src={KurSatu} alt="Kursi 1" className="kotak3" id='kotak3' onClick={algo} />
-</div>
+      <img src={KurSatu} alt="Kursi 1" className="kotak1" id='kotak1' onClick={algo} />
+      <img src={KurSatu} alt="Kursi 1" className="kotak2" id='kotak2' onClick={algo} />
+      <img src={KurSatu} alt="Kursi 1" className="kotak3" id='kotak3' onClick={algo} />
+  </div>
   );
 }
 
